2790a750581998d74f0a5ec741dcd0b2189a527d
Jersy Figueroa <jersyfigueroa27@gmail.com>
2026-03-15 20:02:36 +0100

    Refactor Rectángulo class variable names and methods

--- Rectángulo.java
+++ Rectángulo.java
@@ -1,15 +1,15 @@
 package figuras;
 import java.awt.Color;
 public class Rectángulo extends Figura{
-private double base;
-private double altura;
-public Rectángulo (double x, double y, Color color, double base, double altura){
-super (x, y, color);this.base = base;this.altura = altura;
+private double b;
+private double a;
+public Rectángulo (double x, double y, Color color, double b, double a){
+super (x, y, color);this.b = base;this.a = altura;
 }
-public double getBase(){return base;}
-public double getAltura(){return altura;}
-public void setBase(double base){this.base = base;}
-public void setAltura(double altura){this.altura = altura;}
-public double perímetro (){return 2 * base + 2 * altura;}
-public double área (){return base * altura;}
+public double getBase(){return b;}
+public double getAltura(){return a;}
+public void setBase(double base){this.b = base;}
+public void setAltura(double altura){this.a = altura;}
+public double perímetro (){return 2 * b + 2 * a;}
+public double área (){return b * a;}
 }
